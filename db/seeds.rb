4215ca92ffcac06d06af91e0e8de8350bd63914f
Delie Posnanski <dposnanski@ymail.com>
2017-05-05 21:38:23 +0200

    4.5 changed height of product box

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
-Product.create(:name => "Camera 2 Sticker", :description => "Beautiful camera sticker", :image_url => "sticker2-11", :color => "Black", :price => "65.22")
-
-Product.create(:name => "Good News 2 Sticker", :description => "Beautiful good news sticker", :image_url => "sticker3-11", :color => "Black", :price => "65.22")
