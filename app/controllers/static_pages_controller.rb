c1b685d0b07c8ebd95f3770fbd49f805801128bf
Delie Posnanski <dposnanski@ymail.com>
2017-05-06 20:38:49 +0200

    4.5 dynamic carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   end
   
   def landing_page
-    @products = Product.last(3)
+    @products = Product.last(5)
     render layout: "landing"
   end
   
