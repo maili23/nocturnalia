94c8a2aa740775fa96977544834e80dfd5a52ba6
Delie Posnanski <dposnanski@ymail.com>
2017-05-12 17:01:13 +0200

    5.3 added sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,6 +11,16 @@ class StaticPagesController < ApplicationController
     #@featured_product = Product.first
   #end
   
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'dposnanski@ymail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+  
 end
 
 
