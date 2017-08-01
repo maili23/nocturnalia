ca94b9a291d092fa2b965787d19568a8163651c6
Delie Posnanski <dposnanski@ymail.com>
2017-08-01 10:30:04 +0200

    6.13 added ActionCable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #Javascript instead of CoffeeScript
+  config.app_generators.javascript_engine = :javascript
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :orders, only: [:index, :show, :create, :destroy]
+  mount ActionCable.server => '/cable'
   
   get 'static_pages/about'
 
@@ -27,8 +28,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     delete 'logout', to: 'devise/sessions#destroy'
   end
-
-  mount ActionCable.server => '/cable'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
